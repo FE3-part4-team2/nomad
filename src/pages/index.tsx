ce5405ad3db73f675c6443d951cc3cafef901a5a
jjunhyuki <yjs99004@naver.com>
2024-04-12 19:05:27 +0900

    ⚡️ toast setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
-import styles from '@/styles/Home.module.css';
+import '@/styles/Home.module.css';
+import { toast } from 'react-toastify';
 
 export default function Home() {
-  return <>메인페이지</>;
+  const notify = () => toast.success('toastify test!');
+  const notify1 = () => toast.info('toastify test!');
+  const notify2 = () => toast.error('toastify test!');
+  const notify3 = () => toast.warning('toastify test!');
+
+  return (
+    <>
+      <button className="buttonTest" onClick={notify}>
+        success
+      </button>
+      <button className="buttonTest" onClick={notify1}>
+        info
+      </button>
+      메인페이지
+      <button className="buttonTest" onClick={notify2}>
+        fail
+      </button>
+      <button className="buttonTest" onClick={notify3}>
+        warnning
+      </button>
+    </>
+  );
 }
