3c578a48e1230fa89804bb4e58036c97e84518ad
2zzzyoung <mzooyoungn@gmail.com>
2024-04-15 16:49:10 +0900

    💄 style : 헤더, 푸터 생성

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,15 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <main className={styles.main}>
+        <img src="../assets/images/logo.png" alt="로고 이미지" />
+        <div className={styles.sign}>
+          <a href="/signin">로그인</a>
+          <a href="/signup">회원가입</a>
+        </div>
+      </main>
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
 import styles from '@/styles/Home.module.css';
 
 export default function Home() {
-  return <>메인페이지</>;
+  return (
+    <>
+      <Header />
+      메인페이지
+      <Footer />
+    </>
+  );
 }
