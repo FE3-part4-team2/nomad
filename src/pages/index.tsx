2d25cbf936b457febc383b2ea5c8d546d20a588f
yulmai999 <37208116+yulmai999@users.noreply.github.com>
2024-04-16 22:00:15 +0900

    💄 Design:알람 스타일링
    api기능 제외한 스타일링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,13 @@ import styles from '@/styles/Home.module.css';
 import SideBar from '@/components/SideBar/SideBar';
 import Toast from '@/components/Toast/Toast';
 import { LOGIN_MESSAGES } from '@/constants/message/index';
+import Alarm from '@/components/Alarm/Alarm';
 
 export default function Home() {
   return (
     <>
       <SideBar />
+      <Alarm />
       메인페이지
       <Header />
       메인페이지
