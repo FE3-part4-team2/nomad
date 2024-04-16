4b86e73fcb6f24807a391acaa076e7a6de98acf0
jjunhyuki <yjs99004@naver.com>
2024-04-17 02:34:04 +0900

    Revert "💄 style : 404페이지 css까지 수정 완"
    This reverts commit 082d4979710af0d3dd220c13837824ed0048923e.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,8 +7,8 @@ export default function Header() {
       <main className={styles.main}>
         <img src="../assets/images/logo.png" alt="로고 이미지" />
         <div className={styles.sign}>
-          <Link href="/signin">로그인</Link>
-          <Link href="/signup">회원가입</Link>
+          <Link href="/signin">로그인</a>
+          <Link href="/signup">회원가입</a>
         </div>
       </main>
     </>
---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+const NotFound = () => {
+  return (
+    <>
+      <div className="Container">
+        <div className="notfound-404">
+          <h1>404</h1>
+        </div>
+        <div>
+          <div>Page Not Found</div>
+          <div>페이지를 찾을수 없습니다..</div>
+          <div>
+            <div>존재하지 않는 주소를 입력하셨거나,</div>
+            <div>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</div>
+          </div>
+          <button>
+            <a href="/" role="button">
+              메인으로 돌아가기
+            </a>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default NotFound;
