b3c3f8079d268882e0f0f7c97dffb291d73fdfab
yulmai999 <37208116+yulmai999@users.noreply.github.com>
2024-04-19 06:45:47 +0900

    💄 Design:레이아웃 컴포넌트 디자인
    푸터 스타일 수정& 레이아웃컴포넌트생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,12 @@
-import Footer from '@/components/Footer/Footer';
-import Header from '@/components/Header/Header';
+import Layout from '@/components/Layout/Layout';
 import styles from '@/styles/Home.module.css';
-import SideBar from '@/components/SideBar/SideBar';
 import Toast from '@/components/Toast/Toast';
 import { LOGIN_MESSAGES } from '@/constants/message/index';
-import Alarm from '@/components/Alarm/Alarm';
 
 export default function Home() {
   return (
     <>
-      <SideBar />
-      <Alarm />
-      메인페이지
-      <Header />
-      메인페이지
-      <Footer />
+      <Layout>작동잘되나</Layout>
     </>
   );
 }
