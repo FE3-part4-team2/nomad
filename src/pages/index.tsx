052c14e6f3f14e7672dd8669ea30d6406b76b6b0
2zzzyoung <107063304+2zzzyoung@users.noreply.github.com>
2024-04-24 04:16:55 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,13 @@ import SideBar from '@/components/SideBar/SideBar';
 import Toast from '@/components/Toast/Toast';
 import { LOGIN_MESSAGES } from '@/constants/message/index';
 import Alarm from '@/components/Alarm/Alarm';
-import DetailClass from '@/containers/DetailClass/DetailClass';
 
 export default function Home() {
   return (
     <>
       <Header />
-      {/* <SideBar />
-      <Alarm /> */}
-      <DetailClass />
+      <SideBar />
+      <Alarm />
       <Footer />
     </>
   );
