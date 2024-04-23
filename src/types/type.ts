404d3d54e58d99bc7849d4050d953e7591ec7c39
jihee <sweet9811@gmail.com>
2024-04-23 17:28:48 +0900

    📦 chore: 클래스 데이터 타입 추가

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,1 +1,14 @@
-
+export interface ClassData {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  rating: number;
+  reviewCount: number;
+  createdAt: string;
+  updatedAt: string;
+}
