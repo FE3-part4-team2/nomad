71637ae3fee8d7558539fb6dd779bac739889154
2zzzyoung <107063304+2zzzyoung@users.noreply.github.com>
2024-04-25 19:31:34 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,11 @@ import Layout from '@/components/Layout/Layout';
 import styles from '@/styles/Home.module.css';
 import Toast from '@/components/Toast/Toast';
 import { LOGIN_MESSAGES } from '@/constants/message/index';
-import DetailClass from '@/components/DetailClass/DetailClass';
 
 export default function Home() {
   return (
     <>
-      {/* <Layout>children처리</Layout> */}
-      <DetailClass />
+      <Layout>children처리</Layout>
     </>
   );
 }
