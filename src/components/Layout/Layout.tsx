2903945f6bdfd5b57db4da6bb86b4945bc12cc1c
jjunhyuki <yjs99004@naver.com>
2024-04-30 16:02:54 +0900

    🔀 merge :  브랜치 병합

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,7 @@
 import Footer from '@/components/Footer/Footer';
 import Header from '@/components/Header/Header';
 import SideBar from '@/components/SideBar/SideBar';
-<<<<<<< HEAD
-import styles from './layout.module.css';
-=======
 import styles from './Layout.module.css';
->>>>>>> parent of 5b9186e (💄style :  input 컴포넌트 분리 완)
 
 export default function Layout({ children }) {
   return (
