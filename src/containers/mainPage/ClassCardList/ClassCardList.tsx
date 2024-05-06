79df13cdcd29ee4c2d3b449b0735c9362456e490
jihee <sweet9811@gmail.com>
2024-05-06 17:33:52 +0900

    ✨ feat: 클래스카드 리스트화

---
+++ src/containers/mainPage/ClassCardList/ClassCardList.tsx
@@ -0,0 +1,37 @@
+import styles from './classCardList.module.scss';
+import ClassCard from '@/components/mainPage/ClassCard/ClassCard';
+import CategoryBar from '@/components/mainPage/CategoryBar/CategoryBar';
+import { useEffect, useState } from 'react';
+import { ClassData } from '@/types/type';
+import { getClassListApi } from '@/apis/activitiesApi';
+
+export default function ClassCardList() {
+  const [cardList, setCardList] = useState<ClassData[]>([]);
+
+  async function CardList() {
+    try {
+      const res = await getClassListApi('offset');
+      const cardData = res.activities;
+      setCardList(cardData);
+    } catch (error) {
+      throw error;
+    }
+  }
+  useEffect(() => {
+    CardList();
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <CategoryBar />
+      <h2 className={styles.title}>문화 · 예술</h2>
+      {/* <div className={styles.classCardWrapper}> */}
+      <div className={styles.classCards}>
+        {cardList.map((data, index) => (
+          <ClassCard key={index} classData={data} />
+        ))}
+      </div>
+      {/* </div> */}
+    </div>
+  );
+}
