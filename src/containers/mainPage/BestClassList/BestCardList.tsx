8fc14e247519e3ea05094b8d287ff62ef9202bd5
jihee <sweet9811@gmail.com>
2024-05-06 17:38:49 +0900

    📦 chore: 더미데이터 삭제

--- src/containers/mainPage/BestClassList/BestCardList.tsx
+++ src/containers/mainPage/BestClassList/BestCardList.tsx
@@ -1,15 +1,30 @@
 import BestClassCard from '@/components/mainPage/BestClassCard/BestClassCard';
 import styles from './bestCardList.module.scss';
-import dummyData from '@/apis/dummy';
+import { useEffect, useState } from 'react';
+import { getClassListApi } from '@/apis/activitiesApi';
+import { ClassData } from '@/types/type';
 
 export default function BestCardList() {
-  const cardData = dummyData;
+  const [cardList, setCardList] = useState<ClassData[]>([]);
+
+  async function CardList() {
+    try {
+      const res = await getClassListApi('offset');
+      const cardData = res.activities;
+      setCardList(cardData);
+    } catch (error) {
+      throw error;
+    }
+  }
+  useEffect(() => {
+    CardList();
+  }, []);
 
   return (
     <div>
       <h2 className={styles.title}>인기 체험</h2>
       <div className={styles.bestCards}>
-        {cardData.activities.map((data, index) => (
+        {cardList.map((data, index) => (
           <BestClassCard key={index} classData={data} />
         ))}
       </div>
