c25dbf35a572231759b8a6f4aec88ef465f5dd77
jihee <sweet9811@gmail.com>
2024-05-08 17:09:10 +0900

    ♻️ refactor: 불필요한 부분 삭제 및 사소한 수정

--- src/containers/mainPage/SearchBarContainer/SearchBarContainer.tsx
+++ src/containers/mainPage/SearchBarContainer/SearchBarContainer.tsx
@@ -1,11 +1,8 @@
 import SearchBar from '@/components/mainPage/SearchBar/SearchBar';
-import { searchState } from '@/store/atoms/searchState';
 import { useRouter } from 'next/router';
 import { ChangeEvent, KeyboardEvent, useState } from 'react';
-import { useRecoilState } from 'recoil';
 
 export default function SearchBarContainer() {
-  const [search, setSearch] = useRecoilState(searchState);
   const [inputValue, setInputValue] = useState<string>('');
   const router = useRouter();
 
@@ -17,7 +14,6 @@ export default function SearchBarContainer() {
     if (inputValue === '') {
       //input안이 빈값일때
     } else {
-      setSearch(inputValue);
       router.push(`/search?q=${inputValue}`);
     }
   };
@@ -27,7 +23,6 @@ export default function SearchBarContainer() {
       if (inputValue === '') {
         //input안이 빈값일때
       } else {
-        setSearch(inputValue);
         router.push(`/search?q=${inputValue}`);
       }
     }
