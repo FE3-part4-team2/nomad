4f8b3762a40a66c89507ec323a2b95fa4bf774d3
jihee <sweet9811@gmail.com>
2024-05-13 16:17:45 +0900

    ✨ feat: 배너 슬라이딩 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
 import Header from '@/components/Header/Header';
-import Banner from '@/components/mainPage/Banner/Banner';
 import Footer from '@/components/Footer/Footer';
 import styles from './index.module.scss';
 import BestCardList from '@/containers/mainPage/BestClassList/BestCardList';
 import ClassCardList from '@/containers/mainPage/ClassCardList/ClassCardList';
 import SearchBarContainer from '@/containers/mainPage/SearchBarContainer/SearchBarContainer';
+import BannerContainer from '@/containers/mainPage/Banner/BannerContainer';
 
 export default function Home() {
   return (
     <>
       <Header />
       <div className={styles.banner}>
-        <Banner />
+        <BannerContainer />
       </div>
       <div className={styles.content}>
         <div className={styles.searchBar}>
