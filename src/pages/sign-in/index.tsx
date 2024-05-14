860182793e77f3efd168386d883dd9b5a9c7a78a
jihee <sweet9811@gmail.com>
2024-05-14 13:24:22 +0900

    ♻️ refactor: auth페이지 input 컴포넌트화

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -2,6 +2,7 @@ import styles from './index.module.scss';
 import Button from '@/components/Button/Button';
 import Link from 'next/link';
 import AuthLogoImage from '@/components/logo/AuthLogoImage';
+import AuthInput from '@/components/Input/AuthInput/AuthInput';
 
 export default function SignIn() {
   return (
@@ -9,14 +10,16 @@ export default function SignIn() {
       <div className={styles.container}>
         <AuthLogoImage />
         <div className={styles.inputContainer}>
-          <div className={styles.inputWrapper}>
-            <span>이메일</span>
-            <input type="email" placeholder="이메일을 입력해주세요" />
-          </div>
-          <div className={styles.inputWrapper}>
-            <span>비밀번호</span>
-            <input type="password" placeholder="비밀번호를 입력해주세요" />
-          </div>
+          <AuthInput
+            name="이메일"
+            type="email"
+            placeholder="이메일을 입력해주세요"
+          />
+          <AuthInput
+            name="비밀번호"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
           <div className={styles.loginButton}>
             <Button buttonTitle="로그인 하기" radius={6} fontSize={1.6} />
           </div>
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -2,6 +2,7 @@ import styles from './index.module.scss';
 import Button from '@/components/Button/Button';
 import Link from 'next/link';
 import AuthLogoImage from '@/components/logo/AuthLogoImage';
+import AuthInput from '@/components/Input/AuthInput/AuthInput';
 
 export default function SignUp() {
   return (
@@ -9,25 +10,26 @@ export default function SignUp() {
       <div className={styles.container}>
         <AuthLogoImage />
         <div className={styles.inputContainer}>
-          <div className={styles.inputWrapper}>
-            <span>이메일</span>
-            <input type="email" placeholder="이메일을 입력해주세요" />
-          </div>
-          <div className={styles.inputWrapper}>
-            <span>닉네임</span>
-            <input type="email" placeholder="닉네임을 입력해주세요" />
-          </div>
-          <div className={styles.inputWrapper}>
-            <span>비밀번호</span>
-            <input type="password" placeholder="비밀번호를 입력해주세요" />
-          </div>
-          <div className={styles.inputWrapper}>
-            <span>비밀번호 확인</span>
-            <input
-              type="password"
-              placeholder="비밀번호를 한번 더 입력해주세요"
-            />
-          </div>
+          <AuthInput
+            name="이메일"
+            type="email"
+            placeholder="이메일을 입력해주세요"
+          />
+          <AuthInput
+            name="닉네임"
+            type="email"
+            placeholder="닉네임을 입력해주세요"
+          />
+          <AuthInput
+            name="비밀번호"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
+          <AuthInput
+            name="비밀번호 확인"
+            type="password"
+            placeholder="비밀번호를 한번 더 입력해주세요"
+          />
           <div className={styles.joinButton}>
             <Button buttonTitle="회원가입 하기" radius={6} fontSize={1.6} />
           </div>
