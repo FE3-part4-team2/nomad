36d02bd09825b0ea886a9424d7b2bbdf7ce3a3b4
2zzzyoung <107063304+2zzzyoung@users.noreply.github.com>
2024-05-15 14:04:09 +0900

    ✨ feat : header 드롭다운 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,18 +3,37 @@ import styles from './header.module.scss';
 import Image from 'next/image';
 import { useState } from 'react';
 import { loginApi, loginType } from '../../apis/authApi';
+import { useRouter } from 'next/router';
 
 export default function Header() {
   const [userInfo, setUserInfo] = useState<loginType>();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const router = useRouter();
 
   const onclick = () => {
-    // useEffect(() => {
     const getUserInfo = async () => {
       const res = await loginApi('asd@asd.com', 'asdasdasd');
       setUserInfo(res);
     };
     getUserInfo();
-    // }, []);
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleItemClick = (action: any) => {
+    switch (action) {
+      case '마이페이지':
+        router.push('/my-page');
+        break;
+      case '로그아웃':
+        localStorage.removeItem('accessToken');
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -30,11 +49,13 @@ export default function Header() {
         </Link>
         {userInfo ? (
           <div className={styles.userContainer}>
+            {/* 클릭하면 알림 내역 모달 뜨게 */}
             <Image
               src="/assets/icons/notification.svg"
               alt="알림 아이콘"
               width={20}
               height={20}
+              className={styles.notification}
             />
             <Image
               src="/assets/icons/line.svg"
@@ -43,21 +64,39 @@ export default function Header() {
               height={22}
             />
 
-            {userInfo?.user.profileImageUrl !== null ? (
-              <Image
-                src={userInfo?.user.profileImageUrl}
-                alt="프로필 이미지"
-                width={32}
-                height={32}
-              />
-            ) : (
-              <Image
-                src="/assets/icons/default-user.png"
-                alt="기본 유저 이미지"
-                width={32}
-                height={32}
-              />
-            )}
+            <div className={styles.dropdownContainer} onClick={toggleMenu}>
+              {userInfo?.user.profileImageUrl !== null ? (
+                <Image
+                  src={userInfo?.user.profileImageUrl}
+                  alt="프로필 이미지"
+                  width={32}
+                  height={32}
+                  className={styles.profileImage}
+                />
+              ) : (
+                <Image
+                  src="/assets/icons/default-user.png"
+                  alt="기본 유저 이미지"
+                  width={32}
+                  height={32}
+                  className={styles.profileImage}
+                />
+              )}
+              <div className={styles.dropdownWrapper}>
+                {showMenu && (
+                  <div className={styles.dropdown}>
+                    <ul>
+                      <li onClick={() => handleItemClick('마이페이지')}>
+                        마이페이지
+                      </li>
+                      <li onClick={() => handleItemClick('로그아웃')}>
+                        로그아웃
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
 
             {userInfo?.user.nickname}
           </div>
