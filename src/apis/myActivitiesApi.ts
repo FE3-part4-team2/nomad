0aefbbb04ce43707aa2edd708745658884befa24
eundin <escience1202@gmail.com>
2024-05-15 22:33:58 +0900

    ✨ feat: 내 체험 수정 api 에러 처리

--- src/apis/myActivitiesApi.ts
+++ src/apis/myActivitiesApi.ts
@@ -14,13 +14,32 @@ export const patchEditMyActivityApi = async (
   id: number,
   editMyActivity: PatchEditMyActivityApiType,
 ) => {
-  const res = await axiosInstance.patch(`my-activities/${id}`, editMyActivity, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-  return res.data;
+  try {
+    const res = await axiosInstance.patch(
+      `my-activities/${id}`,
+      editMyActivity,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    return res;
+  } catch (e: any) {
+    if (e.response.status === 401) {
+      toast.error('로그인 해주세요.');
+    } else if (e.response.status === 400) {
+      toast.error('올바른 값을 입력해주세요.');
+    } else if (e.response.status === 409) {
+      toast.error('겹치는 예약 가능 시간대가 존재합니다.');
+    } else if (e.response.status === 403) {
+      toast.error('본인의 체험만 수정할 수 있습니다.');
+    } else if (e.response.status === 404) {
+      toast.error('존재하지 않는 체험입니다.');
+    }
+  }
 };
+
 // 내 체험 삭제
 
 export const deleteActivitiesApi = async (id: number) => {
