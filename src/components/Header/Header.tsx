e82906f6af528053c65d4f2abb9261b86f37b9dc
2zzzyoung <107063304+2zzzyoung@users.noreply.github.com>
2024-05-15 16:31:10 +0900

    ✨ feat : header pr 누락된 부분 추가 및 recoil 타입 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,45 @@
 import Link from 'next/link';
 import styles from './header.module.scss';
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { loginApi } from '../../apis/authApi';
+import { useRouter } from 'next/router';
 import { loginType } from '@/types/authType/AuthType';
 import { userState } from '@/store/atoms/userState';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useSetRecoilState } from 'recoil';
 
 export default function Header() {
   const setUser = useSetRecoilState(userState);
-  const userInfo = useRecoilValue(userState);
+  const [userInfo, setUserInfo] = useState<loginType>();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     const getUserInfo = async () => {
-      const data = await loginApi('1234@1234.com', '123412341234');
-      setUser(data);
-      localStorage.setItem('user', JSON.stringify(data));
+      const res = await loginApi('1234@1234.com', '123412341234');
+      setUserInfo(res);
+      setUser(res);
     };
     getUserInfo();
-  }, [setUser]);
+  }, []);
 
-  const onclick = () => {};
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const handleItemClick = (action: any) => {
+    switch (action) {
+      case '마이페이지':
+        router.push('/my-page');
+        break;
+      case '로그아웃':
+        localStorage.removeItem('accessToken');
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -35,11 +54,13 @@ export default function Header() {
         </Link>
         {userInfo ? (
           <div className={styles.userContainer}>
+            {/* 클릭하면 알림 내역 모달 뜨게 */}
             <Image
               src="/assets/icons/notification.svg"
               alt="알림 아이콘"
               width={20}
               height={20}
+              className={styles.notification}
             />
             <Image
               src="/assets/icons/line.svg"
@@ -48,27 +69,45 @@ export default function Header() {
               height={22}
             />
 
-            {userInfo?.user.profileImageUrl !== null ? (
-              <Image
-                src={userInfo?.user.profileImageUrl}
-                alt="프로필 이미지"
-                width={32}
-                height={32}
-              />
-            ) : (
-              <Image
-                src="/assets/icons/default-user.png"
-                alt="기본 유저 이미지"
-                width={32}
-                height={32}
-              />
-            )}
+            <div className={styles.dropdownContainer} onClick={toggleMenu}>
+              {userInfo?.user.profileImageUrl !== null ? (
+                <Image
+                  src={userInfo?.user.profileImageUrl}
+                  alt="프로필 이미지"
+                  width={32}
+                  height={32}
+                  className={styles.profileImage}
+                />
+              ) : (
+                <Image
+                  src="/assets/icons/default-user.png"
+                  alt="기본 유저 이미지"
+                  width={32}
+                  height={32}
+                  className={styles.profileImage}
+                />
+              )}
+              <div className={styles.dropdownWrapper}>
+                {showMenu && (
+                  <div className={styles.dropdown}>
+                    <ul>
+                      <li onClick={() => handleItemClick('마이페이지')}>
+                        마이페이지
+                      </li>
+                      <li onClick={() => handleItemClick('로그아웃')}>
+                        로그아웃
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </div>
 
             {userInfo?.user.nickname}
           </div>
         ) : (
           <div className={styles.sign}>
-            <button onClick={onclick} />
+            {/* <button onClick={onclick} /> */}
             <Link href="/sign-in">로그인</Link>
             <Link href="/sign-up">회원가입</Link>
           </div>
