7e89a4088b3e171e648123d9df4263701170a357
jihee <sweet9811@gmail.com>
2024-05-16 13:44:05 +0900

    📦 chore: 테마색 변경 및 로고에 링크연결

--- src/components/logo/AuthLogoImage.tsx
+++ src/components/logo/AuthLogoImage.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
 import styles from './authLogoImage.module.scss';
+import Link from 'next/link';
 
 export default function AuthLogoImage() {
   return (
     <div className={styles.logoContainer}>
-      <Image
-        src="/assets/images/iceBreaker.png"
-        alt="로고 이미지"
-        width={330}
-        height={200}
-      />
+      <Link href="/">
+        <Image
+          src="/assets/images/iceBreaker.png"
+          alt="로고 이미지"
+          width={330}
+          height={200}
+        />
+      </Link>
     </div>
   );
 }
