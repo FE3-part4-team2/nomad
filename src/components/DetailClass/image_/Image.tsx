9e8bd58a3928a817fb56888b97144fca431889a8
2zzzyoung <107063304+2zzzyoung@users.noreply.github.com>
2024-06-21 22:07:07 +0900

    💄 design : 모바일 사이즈 메인이미지 수정

--- src/components/DetailClass/image_/Image.tsx
+++ src/components/DetailClass/image_/Image.tsx
@@ -1,5 +1,11 @@
+import { useEffect, useState } from 'react';
 import styles from './image.module.scss';
 import ModalImage from 'react-modal-image';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 interface ImageProps {
   bannerImageUrl: string;
@@ -10,30 +16,98 @@ export default function ImageComponent({
   bannerImageUrl,
   subImages,
 }: ImageProps) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <>
-      <section className={styles.imageSection}>
-        <div className={styles.mainImageWrapper}>
-          <ModalImage
-            small={bannerImageUrl}
-            large={bannerImageUrl}
-            alt="메인이미지"
-            className={styles.mainImage}
-          />
-        </div>
-        <div className={styles.subImageContainer}>
-          {subImages?.slice(0, 4).map((i) => (
-            <div key={i.id} className={styles.subImageWrapper}>
+    <section className={styles.imageSection}>
+      {isMobile ? (
+        <Swiper
+          modules={[Navigation, Pagination]}
+          navigation
+          pagination={{ clickable: true }}
+          slidesPerView={1}
+          spaceBetween={10}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log('slide change')}
+        >
+          <SwiperSlide>
+            <ModalImage
+              small={bannerImageUrl}
+              large={bannerImageUrl}
+              alt="메인이미지"
+              className={styles.mainImage}
+            />
+          </SwiperSlide>
+          {subImages?.map((image) => (
+            <SwiperSlide key={image.id}>
               <ModalImage
-                small={i.imageUrl}
-                large={i.imageUrl}
+                small={image.imageUrl}
+                large={image.imageUrl}
                 alt="서브이미지"
                 className={styles.subImage}
               />
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
-      </section>
-    </>
+        </Swiper>
+      ) : (
+        <>
+          <div className={styles.mainImageWrapper}>
+            <ModalImage
+              small={bannerImageUrl}
+              large={bannerImageUrl}
+              alt="메인이미지"
+              className={styles.mainImage}
+            />
+          </div>
+          <div className={styles.subImageContainer}>
+            {subImages?.slice(0, 4).map((i) => (
+              <div key={i.id} className={styles.subImageWrapper}>
+                <ModalImage
+                  small={i.imageUrl}
+                  large={i.imageUrl}
+                  alt="서브이미지"
+                  className={styles.subImage}
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </section>
+
+    // <>
+    //   <section className={styles.imageSection}>
+    //     <div className={styles.mainImageWrapper}>
+    //       <ModalImage
+    //         small={bannerImageUrl}
+    //         large={bannerImageUrl}
+    //         alt="메인이미지"
+    //         className={styles.mainImage}
+    //       />
+    //     </div>
+    //     <div className={styles.subImageContainer}>
+    //       {subImages?.slice(0, 4).map((i) => (
+    //         <div key={i.id} className={styles.subImageWrapper}>
+    //           <ModalImage
+    //             small={i.imageUrl}
+    //             large={i.imageUrl}
+    //             alt="서브이미지"
+    //             className={styles.subImage}
+    //           />
+    //         </div>
+    //       ))}
+    //     </div>
+    //   </section>
+    // </>
   );
 }
